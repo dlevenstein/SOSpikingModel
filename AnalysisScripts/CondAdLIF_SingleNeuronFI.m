eed406bd357b56a7f3d025d70405c80f5129cf67
dlevenstein <dan.levenstein@gmail.com>
2017-10-24 16:10:52 -0400

    Commenting PopParms

--- AnalysisScripts/CondAdLIF_SingleNeuronFI.m
+++ AnalysisScripts/CondAdLIF_SingleNeuronFI.m
@@ -7,56 +7,56 @@
 repopath = '/Users/dlevenstein/Project Repos/SOSpikingModel'; 
 addpath(genpath(repopath))
 
+figfolder = repopath
+
 %% Define the Population Parameters to feed the FI Cuve function
 
-PopParams.sigma = 10;
+%Input Noise
+PopParams.sigma = 0;        %niose magnitude: variance
+PopParams.theta = 1/10;     %noise time scale (1/ms)
 
-%
-PopParams.EPopNum = 5;
-PopParams.IPopNum = 5;
+% One neuron
+PopParams.EPopNum = 1;
+PopParams.IPopNum = 1;
 
-PopParams.E_L     = -65;
-PopParams.g_L     = 30;
-PopParams.C       = 281;
-PopParams.V_th    = -55;
-PopParams.V_reset = -85;
+%Neuron properties
+PopParams.E_L     = -65;    %rev potential: leak (mV)
+PopParams.g_L     = 30;     %leak conductance (nS?)
+PopParams.C       = 281;    %capacitanve (pF?)
+PopParams.V_th    = -55;    %spike threshold (mV)
+PopParams.V_reset = -85;    %reset potential (mV)
+PopParams.t_ref   = 0.2;    %refractory period (ms)
 
-PopParams.E_e     = 0;
-PopParams.E_i     = -80;
+%Synaptic Properties 
+PopParams.E_e     = 0;      %rev potential: E (mV)
+PopParams.E_i     = -80;    %rev potential: I (mV)
+PopParams.b_s     = 1;      %synaptic decay timescale (1/ms)
+PopParams.ds      = 0.5;    %synaptic activation duration (ms)
+PopParams.a       = 0.5;    %synaptic activation rate (1/ms)
+
+%Adaptation Properties
+PopParams.E_w     = -70;    %rev potential: adaptation (mV)
+PopParams.b_w     = 0.01;   %adaptation decay timescale (1/ms)
+PopParams.dw      = 0.2;    %adaptation activation duration (ms)
+PopParams.b       = 0.1;    %adaptation activation rate (1/ms)
+PopParams.delta_T = 10;     %subthreshold adaptation steepness
+PopParams.w_r = 0.1;        %adaptation at rest (0-1)
+PopParams.gwnorm = 0;       %magnitude of adaptation
+
+%Network Properties
+PopParams.Wee   = 0;        %E->E weight
+PopParams.Wii   = 0;        %I->I weight
+PopParams.Wie   = 0;        %E->I weight
+PopParams.Wei   = 0;        %I->E weight
+PopParams.Kee   = 0;        %Expected E->E In Degree
+PopParams.Kii   = 0;        %Expected I->I In Degree
+PopParams.Kie   = 0;        %Expected E->I In Degree
+PopParams.Kei   = 0;        %Expected I->E In Degree
 
 
-PopParams.E_w     = -70;
-PopParams.b_w     = 0.1;
-
-PopParams.b_s     = 1;
-
-PopParams.dw      = 0.2;
-PopParams.ds      = 0.5;
-
-PopParams.a       = 0.5;
-PopParams.b       = 0.1;
-
-PopParams.t_ref   = 0.2;
-PopParams.delta_T = 10;
-
-PopParams.Wee   = 0;
-PopParams.Wii   = 0;
-PopParams.Wie   = 0;
-PopParams.Wei   = 0; 
-
-PopParams.Kee   = 0;
-PopParams.Kii   = 0;
-PopParams.Kie   = 0;
-PopParams.Kei   = 0;
-
-PopParams.theta = 1/10;
-
-PopParams.gwnorm = 0;
-
-PopParams.w_r = 0.1;
 
 %% Run the FI Curve function
 simfunction = @EMAdLIFfunction;
-Irange = [150 500];
-numI = 11;
+Irange = [150 450];
+numI = 26;
 [ Ivals,rate ] = SimulateFICurve(simfunction,PopParams,Irange,numI);
