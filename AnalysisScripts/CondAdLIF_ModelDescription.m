ac95b5fe22e8c3029057f0481d672d6fcfcbfee0
dlevenstein <dan.levenstein@gmail.com>
2017-10-27 20:56:21 -0400

    added analysis for description and illustration of the CondAdLIF model

---
+++ AnalysisScripts/CondAdLIF_ModelDescription.m
@@ -0,0 +1,20 @@
+%CondAdLIF_ModelDescription.m
+%A script for illustrating the properties of the Conductance-based Adaptive
+%LIF model.
+%%
+
+%% Noise Input Properties
+
+%Magnitude, time scale
+
+%% Voltage Reset
+%refreactory period, step current
+
+%% Adaptation - 
+%subthreshold, superthreshold.  show E cell under low mag step current? w
+%variable, gW.  membrane potential with reversal potential 
+
+%% Synaptic properties and variables
+
+%Show: E spike effect on I, I spike effect on I.  with synaptic variables
+%membrance potential with reversal potentials
