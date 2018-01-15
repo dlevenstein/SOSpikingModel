322cd9a061a5cb9eaf8aa03059c1f9951d47555b
Jonathan Gornet <jmg1030@nyu.edu>
2018-01-15 16:09:23 -0500

    EI Balance Adjustments

--- AnalysisScripts/CondAdLIF_FIWeightMap_BASHSCRIPT.sh
+++ AnalysisScripts/CondAdLIF_FIWeightMap_BASHSCRIPT.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --array=1-21
-#SBATCH --cpus-per-task=10
+#SBATCH --array=1-9
+#SBATCH --cpus-per-task=15
 #SBATCH --time=24:00:00
-#SBATCH --mem=10GB
+#SBATCH --mem=150GB
 #SBATCH --job-name=CondAdLIF_FIWeightMap
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jmg1030@nyu.edu
@@ -14,6 +14,6 @@ i=${SLURM_ARRAY_TASK_ID}
 module purge
 module load matlab/2017a
 
-echo "CondAdLIF_FIWeightMap($i); exit;" | matlab -nodisplay
+echo "CondAdLIF_FIWeightMap($i);" | matlab -nodisplay
 
 echo "Done"
