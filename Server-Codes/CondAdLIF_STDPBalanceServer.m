5729b140a92f08bc56cc65e1af2ee8784b3e574f
Jonathan Gornet <jmg1030@nyu.edu>
2018-05-03 21:57:58 -0400

    Paths issue

--- Server-Codes/CondAdLIF_STDPBalanceServer.m
+++ Server-Codes/CondAdLIF_STDPBalanceServer.m
@@ -4,8 +4,8 @@ function CondAdLIF_STDPBalanceServer(llrr)
 %Path of the SOSpikingModel repository
 
 %repopath = '/Users/dlevenstein/Project Repos/SOSpikingModel'; 
-repopath = '/Users/jonathangornet/Documents/GitHub/SOSpikingModel'; 
-%repopath = '/home/jmg1030/SOSpikingModel'; 
+%repopath = '/Users/jonathangornet/Documents/GitHub/SOSpikingModel'; 
+repopath = '/home/jmg1030/SOSpikingModel'; 
 addpath(genpath(repopath))
 
 %dropboxpath = '/Users/dlevenstein/Dropbox/Share Folders/DLJG'; 
