b2ecd82128414ce599d0acb9704fc7312f218524
Jonathan Gornet <jmg1030@nyu.edu>
2018-09-05 21:07:00 -0400

    Edit Bash Script

--- Server-Codes/STDP_FI_Function.sh
+++ Server-Codes/STDP_FI_Function.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --array=1-800
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=20
 #SBATCH --time=12:00:00
-#SBATCH --mem=20GB
+#SBATCH --mem=62GB
 #SBATCH --job-name=FI_Function_LogWeight
 #SBATCH --mail-type=END
 #SBATCH --mail-user=jmg1030@nyu.edu
