5751eec4dd73818ee66b34bad7850939bfec6fd8
Jonathan Gornet <jmg1030@nyu.edu>
2019-01-29 15:19:22 -0800

    Adaptation for server

---
+++ Server-Codes/ExperimentRate/Adaptation/CondAdLIF_STDP_ExperimentRate_Adaptation.m
@@ -0,0 +1,53 @@
+function CondAdLIF_STDP_ExperimentRate_Adaptation(ii)
+
+repopath = '/scratch/jmg1030/FIcurve/SOSpikingModel';
+addpath(genpath(repopath))
+
+load('/scratch/jmg1030/FIcurve/data/trainedWeights/ExperimentRate/ExperimentRate_s_10.mat');
+
+PopParams_in = PopParams;
+PopParams_in.tau_w = 100;
+PopParams_in.LearningRate = 0;
+PopParams_in.sigma = 10;
+PopParams_in.W = SimValues.WeightMat(:,:,end);
+PopParams_in.gwnorm = PopParams_in.g_L(1);
+
+PopParams_in.V0 = min(PopParams_in.E_L) + (min(PopParams_in.V_th)-min(PopParams_in.E_L)).*rand(PopParams_in.EPopNum + PopParams_in.IPopNum,1);
+
+TimeParams.dt      = 0.05;
+TimeParams.SimTime = 1e4;
+
+Ivals = linspace(0,400,21);
+bvals = [0 logspace(-1,2,9)];
+
+I_e = Ivals(ii);
+    
+parfor bb = 1:length(bvals)
+    
+ii
+bb 
+
+PopParamsAnalysis = PopParams_in;
+
+b = bvals(bb);
+
+PopParamsAnalysis.I_e = I_e;
+PopParamsAnalysis.b = b;
+
+SimValuesArray(ii,bb) = AdLIFfunction_STDP(PopParamsAnalysis,TimeParams,'cellout',true,'showprogress',false,'showfig',false,'save_dt',1,'save_weights',TimeParams.SimTime);
+
+end
+for bb = 1:length(bvals)
+    
+    SimValues.spikes = SimValuesArray(ii,bb).spikes;
+    SimValues.V = SimValuesArray(ii,bb).V;
+    
+    SimValues.g_e = SimValuesArray(ii,bb).g_e;
+    SimValues.g_i = SimValuesArray(ii,bb).g_i;
+    SimValues.g_w = SimValuesArray(ii,bb).g_w;
+    
+    save(['/scratch/jmg1030/FIcurve/data/bistabilityTest/Adaptation/ExperimentRate/AdaptationVCurrent_ii_' num2str(ii) '_bb_' num2str(bb) '.mat'],'SimValues','-struct','-v7.3') 
+    
+end
+
+end
---
+++ Server-Codes/ExperimentRate/Adaptation/STDP_Experiment_Adaptation.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=Adaptation
+#SBATCH --nodes=1
+#SBATCH --array=1-21
+#SBATCH --cpus-per-task=10
+#SBATCH --time=100:00:00
+#SBATCH --mem=62GB
+#SBATCH --job-name=Adaptation
+#SBATCH --output=Adaptation_%j.out
+#SBATCH --mail-type=END
+#SBATCH --mail-user=jmg1030@nyu.edu
+
+n=${SLURM_ARRAY_TASK_ID}
+
+module purge
+module load matlab/2018a
+
+if [ "$SLURM_JOBTMP" == "" ]; then
+    export SLURM_JOBTMP=/state/partition1/$USER/$$
+    mkdir -p $SLURM_JOBTMP
+fi
+
+export MATLAB_PREFDIR=$(mktemp -d $SLURM_JOBTMP/matlab-XXXX)
+
+echo
+echo "Hostname: $(hostname)"
+echo
+
+cat<<EOF | srun matlab -nodisplay
+parpool('local', $SLURM_CPUS_PER_TASK)
+CondAdLIF_STDP_ExperimentRate_Adaptation(${n})
+exit
+EOF
+
+rm -rf $SLURM_JOBTMP/*
