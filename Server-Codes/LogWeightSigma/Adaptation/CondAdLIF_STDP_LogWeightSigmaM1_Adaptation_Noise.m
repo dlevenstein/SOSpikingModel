f314154495698576a526a4d9e41ca982a56c140f
Jonathan Gornet <jmg1030@nyu.edu>
2019-03-20 22:38:14 -0700

    Run Simulation shorter

--- Server-Codes/LogWeightSigma/Adaptation/CondAdLIF_STDP_LogWeightSigmaM1_Adaptation_Noise.m
+++ Server-Codes/LogWeightSigma/Adaptation/CondAdLIF_STDP_LogWeightSigmaM1_Adaptation_Noise.m
@@ -15,7 +15,7 @@ PopParams_in.gwnorm = PopParams_in.g_L(1);
 PopParams_in.V0 = min(PopParams_in.E_L) + (min(PopParams_in.V_th)-min(PopParams_in.E_L)).*rand(PopParams_in.EPopNum + PopParams_in.IPopNum,1);
 
 TimeParams.dt      = 0.05;
-TimeParams.SimTime = 4e4;
+TimeParams.SimTime = 2e4;
 
 Ivals = linspace(100,300,41);
 bvals = 0:10:100;
@@ -34,7 +34,7 @@ b = bvals(bb);
 PopParamsAnalysis.I_e = I_e;
 PopParamsAnalysis.b = b;
 
-SimValuesArray = AdLIFfunction_STDP(PopParamsAnalysis,TimeParams,'cellout',true,'showprogress',false,'showfig',false,'save_dt',TimeParams.SimTime,'save_weights',TimeParams.SimTime,'onsettime',3e4);
+SimValuesArray = AdLIFfunction_STDP(PopParamsAnalysis,TimeParams,'cellout',true,'showprogress',false,'showfig',false,'save_dt',TimeParams.SimTime,'save_weights',TimeParams.SimTime,'onsettime',1e4);
 
 save(['/scratch/jmg1030/FIcurve/data/bistabilityTest/Adaptation/LogWeightSigmaM1/AdaptationVCurrentSpikes_ii_' num2str(ii) '_bb_' num2str(bb) '.mat'],'SimValuesArray.spikes','-v7.3') 
 
