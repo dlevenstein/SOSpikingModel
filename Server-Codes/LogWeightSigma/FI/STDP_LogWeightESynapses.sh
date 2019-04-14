83672d2ac7a3bab15b97f27b4fbcc63196042cbf
Jonathan Gornet <jmg1030@nyu.edu>
2019-04-14 18:16:41 -0400

    Unneeded file

--- Server-Codes/LogWeightSigma/FI/STDP_LogWeightESynapses.sh
+++ Server-Codes/LogWeightSigma/FI/STDP_LogWeightESynapses.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --array=1-4
-#SBATCH --cpus-per-task=2
-#SBATCH --time=100:00:00
-#SBATCH --mem=40GB
 #SBATCH --job-name=LogWeightESynapses
+#SBATCH --nodes=1
+#SBATCH --array=1-5
+#SBATCH --cpus-per-task=10
+#SBATCH --time=100:00:00
+#SBATCH --mem=10GB
+#SBATCH --job-name=LogWeightESynapses
+#SBATCH --output=LogWeightESynapses_%j.out
 #SBATCH --mail-type=END
 #SBATCH --mail-user=jmg1030@nyu.edu
-#SBATCH --output=LogWeightESynapses_%j.out
-#SBATCH --gres=gpu:k80:1
 
 n=${SLURM_ARRAY_TASK_ID}
 
 module purge
 module load matlab/2018a
 
-echo "CondAdLIF_STDP_FI_Function_LogWeightESynapses(${n})" | matlab
+if [ "$SLURM_JOBTMP" == "" ]; then
+    export SLURM_JOBTMP=/state/partition1/$USER/$$
+    mkdir -p $SLURM_JOBTMP
+fi
 
-echo "done"
+export MATLAB_PREFDIR=$(mktemp -d $SLURM_JOBTMP/matlab-XXXX)
+
+echo
+echo "Hostname: $(hostname)"
+echo
+
+cat<<EOF | srun matlab -nodisplay
+parpool('local', $SLURM_CPUS_PER_TASK)
+CondAdLIF_STDP_FI_Function_LogWeightESynapses(${n})
+exit
+EOF
+
+rm -rf $SLURM_JOBTMP/*
