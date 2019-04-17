24c8054a411b867942d431db20e64f08ebf33309
Jonathan Gornet <jmg1030@nyu.edu>
2019-04-17 10:17:15 -0400

    Resource management

--- Server-Codes/LogWeightSigma/Adaptation/STDP_LogWeightSigmaM1_Adaptation.sh
+++ Server-Codes/LogWeightSigma/Adaptation/STDP_LogWeightSigmaM1_Adaptation.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --array=1-4
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=20
 #SBATCH --time=100:00:00
 #SBATCH --mem=40GB
 #SBATCH --job-name=AdaptationLogWeight
 #SBATCH --mail-type=END
 #SBATCH --mail-user=jmg1030@nyu.edu
 #SBATCH --output=AdaptationLogWeight_%j.out
-#SBATCH --gres=gpu:k80:1
 
 n=${SLURM_ARRAY_TASK_ID}
 
