7f04e58a595a99e122d25e6a041b9392f5a81965
Jonathan Gornet <jmg1030@nyu.edu>
2019-04-17 10:15:58 -0400

    Use CPU instead

--- Server-Codes/LogWeightSigma/Adaptation/CondAdLIF_STDP_LogWeightSigmaM1_Adaptation_Noise.m
+++ Server-Codes/LogWeightSigma/Adaptation/CondAdLIF_STDP_LogWeightSigmaM1_Adaptation_Noise.m
@@ -9,7 +9,7 @@ PopParams_in = PopParams;
 PopParams_in.tau_w = 300;
 PopParams_in.LearningRate = 0;
 PopParams_in.sigma = 25;
-PopParams_in.W = gather(SimValues.WeightMat(:,:,end));
+PopParams_in.W = SimValues.WeightMat(:,:,end);
 PopParams_in.gwnorm = PopParams_in.g_L(1);
 PopParams_in.t_syn = 0;
 
@@ -22,7 +22,7 @@ TimeParams.SimTime = 1e4;
 Ivals = linspace(100,300,21);
 bvals = 0:10:100;
 
-for II = 1:(length(Ivals)*length(bvals))
+parfor II = 1:(length(Ivals)*length(bvals))
     
 if mod(II,4)+1 == index
 ii = mod(II,length(Ivals))+1;
@@ -40,7 +40,7 @@ PopParamsAnalysis.I_e = I_e;
 PopParamsAnalysis.b = b;
 
 SimValuesArray = AdLIFfunction_STDP_GPU(PopParamsAnalysis,TimeParams,'cellout',true,'showprogress',true,'showfig',false,...
-    'save_weights',SimTime,'save_dt',SimTime,'useGPU',true);
+    'save_weights',SimTime,'save_dt',SimTime,'useGPU',false);
 
 spikes = SimValuesArray.spikes;
 
