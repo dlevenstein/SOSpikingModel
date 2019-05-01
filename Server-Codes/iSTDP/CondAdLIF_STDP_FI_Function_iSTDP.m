d3b538494eca290f4399d44beae64eb50f1f061a
Jonathan Gornet <jmg1030@nyu.edu>
2019-04-30 23:27:34 -0400

    Adjusting folders

--- Server-Codes/iSTDP/CondAdLIF_STDP_FI_Function_iSTDP.m
+++ Server-Codes/iSTDP/CondAdLIF_STDP_FI_Function_iSTDP.m
@@ -43,7 +43,7 @@ PopParams.tauSTDP = 20;
 
 %%
 
-load('/scratch/jmg1030/FIcurve/data/iSTDPTrainedWeights/Lognormal_m_1_s_10_iSTDP.mat');
+load('/scratch/jmg1030/FIcurve/data/iSTDPTrainedWeights/Uniform_w_1_iSTDP_IE_indegree_500.mat');
 
 for simnum = 1:100
    
@@ -61,8 +61,8 @@ PopParamsAnalysis.W = SimValues.WeightMat(:,:,end);
 
 PopParamsAnalysis.V0 = min(PopParamsAnalysis.E_L) + (min(PopParamsAnalysis.V_th)-min(PopParamsAnalysis.E_L)).*rand(PopParamsAnalysis.EPopNum + PopParamsAnalysis.IPopNum,1);
 
-datafolder = '/scratch/jmg1030/FIcurve/data/bistabilityTest/DOWN/iSTDPLognormal/';
-dataname = ['iSTDP_Lognormal_m_1_s_10_sim_' char(num2str(simnum))];
+datafolder = '/scratch/jmg1030/FIcurve/data/bistabilityTest/DOWN/Uniform_w_1/';
+dataname = ['Uniform_w_1_K_IE_1000_sim_' char(num2str(simnum))];
 
 disp([datafolder dataname]);
 
