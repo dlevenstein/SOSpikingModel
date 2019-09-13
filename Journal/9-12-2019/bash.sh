dba5da711ae25b3ec75bdf27fb9c0e682822cecf
Jonathan Gornet <jmg1030@nyu.edu>
2019-09-13 14:01:18 -0500

    resource management

--- Journal/9-12-2019/bash.sh
+++ Journal/9-12-2019/bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --array=1-2
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=10
 #SBATCH --time=100:00:00
 #SBATCH --mem=62GB
 #SBATCH --job-name=iSTDPTrainingLongClusters
