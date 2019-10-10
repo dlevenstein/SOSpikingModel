3df258268fb1acebc4dd9152b41745f01ae28d84
Jonathan Gornet <jmg1030@nyu.edu>
2019-10-09 21:09:49 -0500

    file errors

--- Journal/10-9-2019/Testing/CondAdLIF_iSTDP_Testing.m
+++ Journal/10-9-2019/Testing/CondAdLIF_iSTDP_Testing.m
@@ -9,7 +9,7 @@ sVals = ["01","1","10"];
     
 %%
 
-load(['/scratch/jmg1030/FIcurve/data/trainedWeights/LogWeightSigmaM1/Lognormal_m_1_s_' char(sVals(modnum)) '.mat']);
+load(['/scratch/jmg1030/FIcurve/data/trainedWeights/LogWeightSigmaM1/logWeight_m_1_s_' char(sVals(modnum)) '.mat']);
 
 %%
 clear PopParams
@@ -67,6 +67,6 @@ PopParams_in.I_e       = 250;
 SimValues = AdLIFfunction_iSTDP(PopParams_in,TimeParams,'cellout',true,'showprogress',true,'showfig',false,...
     'save_weights',TimeParams.SimTime,'save_dt',1,'useGPU',false,'defaultNeuronParams',false);
 
-save(['/scratch/jmg1030/FIcurve/data/trainedWeights/LogWeightSigmaM1/Testing_Lognormal_m_1_s_' char(sVals(modnum)) '.mat'],'SimValues','-v7.3') 
+save(['/scratch/jmg1030/FIcurve/data/trainedWeights/LogWeightSigmaM1/Testing_logWeight_m_1_s_' char(sVals(modnum)) '.mat'],'SimValues','-v7.3') 
 
 end
