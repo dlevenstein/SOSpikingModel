306fddf80a8e3cdc62150b5e70be961c3a51e039
dlevenstein <dan.levenstein@gmail.com>
2019-10-09 20:06:20 -0400

    back to 500

--- AnalysisScripts/Adaptation_Examples/CondAdLIF_iSTDP_Adaptation_Current.m
+++ AnalysisScripts/Adaptation_Examples/CondAdLIF_iSTDP_Adaptation_Current.m
@@ -68,7 +68,7 @@ PopParams.tauSTDP = 20;
 
 PopParamsAnalysis              = PopParams;
 PopParamsAnalysis.LearningRate = 0;                      %Learning rate
-PopParamsAnalysis.tau_w        = 1000;    %300                %adaptation decay (ms)
+PopParamsAnalysis.tau_w        = 500;    %300                %adaptation decay (ms)
 PopParamsAnalysis.sigma        = 100;     %100               %Noise variance (pA) (Set to Covariance Matrix to add covariance
 PopParamsAnalysis.W            = SimValues.WeightMat(:,:,end); %Synaptic Weights
 PopParamsAnalysis.gwnorm       = 1;                      %Adaptation norm
